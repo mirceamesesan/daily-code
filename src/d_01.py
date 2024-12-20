6716376e6e331e2646bb23cb7f751e85b677d894
Mircea Mesesan <mircea.mesesan@yahoo.com>
2024-12-20 22:07:02 +0100

    day four, with flask started

--- src/d_01.py
+++
@@ -1,7 +0,0 @@
-import requests
-import pandas as pd
-
-def day_one():
-    print("\nDAY ONE ROLLING!")
-    print("Pandas version: ", pd.__version__)
-    print("Requests version: ", requests.__version__)
