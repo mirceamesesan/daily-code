8a4a274e95c6c2772b5f2d76c573c432ee413ca4
Mircea Mesesan <mircea.mesesan@yahoo.com>
2024-12-21 14:11:37 +0100

    d4

--- src/d_04.py
+++ src/d_04.py
@@ -1,9 +1,19 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+def get_time_entries():
+    """Retrieving all our time entries from the database"""
+    # response = requests.get(url="http://localhost:8000")
+    # print(response.json())
+    print("Retrieving time entries...")
+
+
 @app.route("/")
 def home():
+    get_time_entries()
     return render_template("home.html")
 
 
